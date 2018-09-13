83349f3edb05388b1e2fcbe54b86399780b29a30
Antoniya Statelova <ats@darksky.net>
2018-09-13 18:05:11 -0400

    Add tests (which work consistently between old and new logic, remove old logic

--- test/lunar.js
+++ test/lunar.js
@@ -6,25 +6,92 @@ const astro  = require("../index"),
 describe("lunar", () => {
   /* FIXME: lots of methods to test */
 
-  /*http://aa.usno.navy.mil/rstt/onedaytable
-   *  ?ID=AA&year=2006&month=3&day=20&state=NM&place=Albuquerque */
-  const date = new Date("2006-03-20T19:06:28.800Z"),
-      t    = astro.date_to_julian(date),
-      lat  = 35.05 * (Math.PI / 180),
-      lon  = -106.62 * (Math.PI / 180);
+  /* http://aa.usno.navy.mil/data/docs/RS_OneDay.php */
+  const inputs = [
+      {
+        // Albuquerque
+        t: astro.date_to_julian(new Date("2006-03-20T19:06:28.800Z")),
+        lat: 35.05,
+        lon: -106.62
+      },
+      {
+          // NYC starting at midnight for some day...
+          t: astro.date_to_julian(new Date("2016-03-13T00:00:00-0500")),
+          lat: 40.71,
+          lon: -74.01
+      },
+      {
+          // NYC ... later that day so we can get a diff moonrise time!
+          t: astro.date_to_julian(new Date("2016-03-13T12:00:00-0500")),
+          lat: 40.71,
+          lon: -74.01
+      },
+      {
+          // NYC ... a different period entirely...
+          t: astro.date_to_julian(new Date("2017-12-01T00:00:00-0500")),
+          lat: 40.71,
+          lon: -74.01
+      },
+      {
+          // NYC ... and forward in that period so we get diff transit ...
+          t: astro.date_to_julian(new Date("2017-12-01T22:30:00-0500")),
+          lat: 40.71,
+          lon: -74.01
+      },
+    ];
+
+  const rises = [
+    Date.parse("2006-03-21T00:16-0700"),
+    Date.parse("2016-03-13T10:17-0400"),
+    Date.parse("2016-03-14T11:04-0400"),
+    Date.parse("2017-12-01T15:27-0500"),
+    Date.parse("2017-12-02T16:10-0500"),
+  ];
+
+  const transits = [
+    Date.parse("2006-03-20T04:08-0700"),
+    Date.parse("2016-03-12T15:28-0500"),
+    Date.parse("2016-03-12T15:28-0500"),
+    Date.parse("2017-11-30T21:27-0500"),
+    Date.parse("2017-12-01T22:22-0500"),
+  ];
+
+  const sets = [
+    Date.parse("2006-03-21T09:45-0700"),
+    Date.parse("2016-03-14T00:31-0400"),
+    Date.parse("2016-03-14T00:31-0400"),
+    Date.parse("2017-12-01T04:13-0500"),
+    Date.parse("2017-12-02T05:26-0500"),
+  ];
+
+  function _check_calculation(input, result, lookup_function) {
+    const calculation = lookup_function(
+        input.t,
+        input.lat * (Math.PI / 180),
+        input.lon * (Math.PI / 180)
+    );
+    expect(
+        astro.julian_to_date(calculation).getTime()
+    ).to.be.closeTo(result, 10 * 60 * 1000); // Allow error of 10min
+  }
 
   it("should return the next moonrise to a given time", () => {
-    expect(astro.julian_to_date(lunar.rise(t, lat, lon)).getTime()).
-      to.be.closeTo(Date.parse("2006-03-21T00:16-0700"), 420000);
+    for(let i = 0; i < rises.length; i++) {
+      _check_calculation(inputs[i], rises[i], lunar.rise);
+    }
   });
 
   it("should return the nearest lunar transit to a given time", () => {
-    expect(astro.julian_to_date(lunar.transit(t, lat, lon)).getTime()).
-      to.be.closeTo(Date.parse("2006-03-20T04:08-0700"), 420000);
+    for(let i = 0; i < transits.length; i++) {
+        _check_calculation(inputs[i], transits[i], lunar.transit);
+    }
   });
 
   it("should return the next moonset to a given time", () => {
-    expect(astro.julian_to_date(lunar.set(t, lat, lon)).getTime()).
-      to.be.closeTo(Date.parse("2006-03-21T09:45-0700"), 420000);
+    for(let i = 0; i < sets.length; i++) {
+        _check_calculation(inputs[i], sets[i], lunar.set);
+    }
   });
+
+
 });
